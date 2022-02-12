8600d1af447fcd8e9207d5c29b9843e51c6065a3
hazal Çelik <hazal.celik@hotmail.com.tr>
2022-02-13 02:22:30 +0300

    GenericRepository Metotları yazıldı.

--- Rise.Repository/Seeds/PersonContactSeed.cs
+++ Rise.Repository/Seeds/PersonContactSeed.cs
@@ -15,7 +15,7 @@ namespace Rise.Repository.Seeds
         {
             builder.HasData(
                 new PersonContact { Id = 1, CityId = 1, Email = "hazal@hotmail.com", Phone = "05512365659", PersonId = 1 },
-                new PersonContact { Id = 2, CityId = 5, Email = "Sinem@hotmail.com", Phone = "05512695659", PersonId = 2 },
+                new PersonContact { Id = 2, CityId = 5, Email = "Sinem@hotmail.com", Phone = "05512695659", PersonId = 2 }
                 );
         }
     }
