828c401bde0f0a1aa7cf7f5e0d90deaa9de6a2d5
hazal Çelik <hazal.celik@hotmail.com.tr>
2022-02-13 03:22:47 +0300

    Migration ve Service katmanı tamamlandı.

--- Rise.Repository/Repositories/GenericRepository.cs
+++ Rise.Repository/Repositories/GenericRepository.cs
@@ -34,7 +34,7 @@ namespace Rise.Repository.Repositories
            return await _dbSet.AnyAsync(expression);
         }
 
-        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        public IQueryable<T> GetAll()
         {
             return _dbSet.AsNoTracking().AsQueryable();
         }
