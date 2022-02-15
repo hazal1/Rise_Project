b50406cbdd7767d3e175d95b3e82a0e71bcc5b19
hazal Çelik <hazal.celik@hotmail.com.tr>
2022-02-15 22:52:39 +0300

    Kişileri Listeleme

--- Rise.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Rise.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
